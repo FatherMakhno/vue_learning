f32066ce1c3dec814720cb9bf8e00110d24a06ad
Daniil <dolhovskiy23@gmail.com>
2024-01-25 09:50:06 +0200

    first commit

--- vue_project_1/src/App.vue
+++ vue_project_1/src/App.vue
@@ -1,10 +1,34 @@
 <script>
-
+  export default {
+    data() {
+      return {
+        users: [],
+        userName: '',
+        userPass: '',
+        userEmail: ''
+      }
+    },
+    methods: {
+      sendData() {
+        this.users.push({
+          name: this.userName,
+          pass: this.userPass,
+          emai: this.userEmail
+        })
+      }
+    }
+  }
 </script>
 
 <template>
-  <h1>test</h1>
+  <input type="text" v-model="userName" placeholder="Имя">
+  <input type="password" v-model="userPass" placeholder="Пароль">
+  <input type="email" v-model="userEmail" placeholder="Email">
+  <p className="error"></p>
+  <button @click="sendData()">Отправить</button>
+  <p>{{ users }}</p>
 </template>
-<style scoped>
 
+<style scoped>
+  
 </style>
