3f4557195bc01c3182f1a601f66f48ca4d516e2f
Daniil <dolhovskiy23@gmail.com>
2024-01-26 17:15:47 +0200

    с ошибкой

--- crypto_change/src/App.vue
+++ crypto_change/src/App.vue
@@ -1,11 +1,15 @@
-<template>
-  
-</template>
-
 <script>
-
+// import Input from  './components/Input.vue'
+// export default {
+//   components: { Input }
+// }
 </script>
 
-<style scoped>
+<template>
+  <h1>Crypto</h1>
+  <!-- <Input /> -->
+</template>
+
+<style>
 
 </style>
---
+++ crypto_change/src/components/Input.vue
@@ -0,0 +1,14 @@
+/* eslint-disable */
+<!-- <script>
+export default {
+
+}
+</script>
+
+<template>
+  <input type="number" placeholder="ввод">
+</template>
+
+<style>
+
+</style> -->
